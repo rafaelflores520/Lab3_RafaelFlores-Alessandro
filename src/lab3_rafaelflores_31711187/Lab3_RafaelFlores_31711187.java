7ce08514a585c5351ffdef664484f3e6102c9227
agreyest <56709605+agreyest@users.noreply.github.com>
2019-10-25 14:08:43 -0600

    push_AR

--- src/lab3_rafaelflores_31711187/Lab3_RafaelFlores_31711187.java
+++ src/lab3_rafaelflores_31711187/Lab3_RafaelFlores_31711187.java
@@ -2,6 +2,7 @@
     Lab3 - Herencia
  */
 package lab3_rafaelflores_31711187;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab3_RafaelFlores_31711187 {
@@ -9,6 +10,8 @@ public class Lab3_RafaelFlores_31711187 {
     
     public static void main(String[] args) {
         char resp = 's';
+        ArrayList <Ejercicto> ejercito = new ArrayList();
+        ArrayList <Arma> armas = new ArrayList();
         while(resp == 's'){
             System.out.println("1- Agregar ejercito"
                     + "\n2- Seleccionar armas para comprar"
@@ -17,6 +20,29 @@ public class Lab3_RafaelFlores_31711187 {
                     + "\n5- Modificar ejercito"
                     + "\n6- Jugar"
                     + "\n7- Salir");
+            int opcion = read.nextInt();
+            switch(opcion){
+                case 1:
+                    System.out.print("");
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    resp = 'n';
+            }
         }//fin del while
         
     }//fin del main
