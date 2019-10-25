0dcf382e961c2d1419ec89e862da98967b7839b0
agreyest <56709605+agreyest@users.noreply.github.com>
2019-10-25 14:03:17 -0600

    push2_Ar

--- src/lab3_rafaelflores_31711187/Lab3_RafaelFlores_31711187.java
+++ src/lab3_rafaelflores_31711187/Lab3_RafaelFlores_31711187.java
@@ -8,6 +8,16 @@ public class Lab3_RafaelFlores_31711187 {
     public static Scanner read = new Scanner(System.in);
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
+        char resp = 's';
+        while(resp == 's'){
+            System.out.println("1- Agregar ejercito"
+                    + "\n2- Seleccionar armas para comprar"
+                    + "\n3- Listar arma"
+                    + "\n4- Modificar arma"
+                    + "\n5- Modificar ejercito"
+                    + "\n6- Jugar"
+                    + "\n7- Salir");
+        }//fin del while
+        
+    }//fin del main
 }
