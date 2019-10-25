098535efcc315e87ac10ede591ce307fb6b1397b
Rafael <rafac@unitec.edu>
2019-10-25 17:07:11 -0600

    Primer Subidon

---
+++ src/lab3_rafaelflores_31711187/Lab3_RafaelFlores_31711187.java
@@ -0,0 +1,13 @@
+/*
+    Lab3 - Herencia
+ */
+package lab3_rafaelflores_31711187;
+import java.util.Scanner;
+
+public class Lab3_RafaelFlores_31711187 {
+    public static Scanner read = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
